e08fa82d2849d4cafa2ec8ca3eed0f7c693d02bb
gvinianidzegivi <gvinianidzegivi@gmail.com>
2021-01-20 18:12:05 +0400

    camera module

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,12 +1,69 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, View, TouchableHighlight, Text} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
-const Add = () => {
+const Add = (props) => {
+  androidCameraPermissionOptions = {
+    title: 'Permission to use camera',
+    message: 'We need your permission to use your camera',
+    buttonPositive: 'Ok',
+    buttonNegative: 'Cancel',
+  };
+
+  const cameraButtonPressed = async () => {
+    if (camera) {
+      const options = {
+        quality: 0.5,
+        base64: true,
+        orientation: 'portrait',
+        pauseAfterCapture: true,
+        fixOrientation: true,
+      };
+      const data = await camera.takePictureAsync(options);
+      console.log(data.uri);
+      //   props.onPictureTaken(data.uri);
+    }
+  };
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <RNCamera
+        ref={(ref) => {
+          camera = ref;
+        }}
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        androidCameraPermissionOptions={androidCameraPermissionOptions}
+        captureAudio={false}></RNCamera>
+      <TouchableHighlight
+        style={styles.shutter}
+        onPress={() => cameraButtonPressed()}>
+        <Text style={styles.shutterText}></Text>
+      </TouchableHighlight>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  preview: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+  shutter: {
+    borderRadius: 50,
+    height: 70,
+    width: 70,
+    backgroundColor: '#FFFFFF',
+    position: 'absolute',
+    bottom: 50,
+    zIndex: 100,
+    alignSelf: 'center',
+  },
+});
 
 export default Add;
